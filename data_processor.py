e008dbc7a006059d7e3934b6815028a190669cb5
kelvi <ckmartin1995@gmail.com>
2026-04-20 08:45:11 -0400

    Update data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -3,7 +3,9 @@ import pandas as pd
 def fetch_and_clean():
     url = "https://genconreg-production.s3.amazonaws.com/exports/events.xlsx"
     df = pd.read_excel(url)
-    df['Day'] = pd.to_datetime(df['Start Date & Time']).dt.day_name()
+    dt_objs = pd.to_datetime(df['Start Date & Time'])
+    df['Day'] = dt_objs.dt.day_name()
+    df['Time'] = dt_objs.dt.strftime('%I:%M %p')
     df.to_json('events.json', orient='records')
 
 if __name__ == "__main__":
