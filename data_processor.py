69dbf422d5fd14f44a5e9da92aacd561b02914a8
kelvi <ckmartin1995@gmail.com>
2026-04-20 08:33:47 -0400

    change start time to date, add system tab

--- data_processor.py
+++ data_processor.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 def fetch_and_clean():
-    df = pd.read_excel("https://genconreg-production.s3.amazonaws.com/exports/events.xlsx")
+    url = "https://genconreg-production.s3.amazonaws.com/exports/events.xlsx"
+    df = pd.read_excel(url)
     df['Day'] = pd.to_datetime(df['Start Date & Time']).dt.day_name()
     df.to_json('events.json', orient='records')
 
