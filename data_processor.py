f0fa3ec4419afac2e5f2a82239f6d42c0b7f86d9
kelvi <ckmartin1995@gmail.com>
2026-04-23 07:49:31 -0400

    added change log

--- data_processor.py
+++ data_processor.py
@@ -1,12 +1,43 @@
 import pandas as pd
+import json
+import os
+from datetime import datetime
 
 def fetch_and_clean():
     url = "https://genconreg-production.s3.amazonaws.com/exports/events.xlsx"
-    df = pd.read_excel(url)
-    dt_objs = pd.to_datetime(df['Start Date & Time'])
-    df['Day'] = dt_objs.dt.day_name()
-    df['Time'] = dt_objs.dt.strftime('%I:%M %p')
-    df.to_json('events.json', orient='records')
+    new_df = pd.read_excel(url)
+    
+    dt_objs = pd.to_datetime(new_df['Start Date & Time'])
+    new_df['Day'] = dt_objs.dt.day_name()
+    new_df['Time'] = dt_objs.dt.strftime('%I:%M %p')
+
+    changelog_file = 'changelog.json'
+    history = []
+    if os.path.exists(changelog_file):
+        with open(changelog_file, 'r') as f:
+            history = json.load(f)
+
+    if os.path.exists('events.json'):
+        old_df = pd.read_json('events.json')
+        
+        new_ids = set(new_df['Game ID']) - set(old_df['Game ID'])
+
+        removed_ids = set(old_df['Game ID']) - set(new_df['Game ID'])
+        
+        if new_ids or removed_ids:
+            history.insert(0, {
+                "timestamp": datetime.now().strftime("%Y-%m-%d %I:%M %p"),
+                "added": len(new_ids),
+                "removed": len(removed_ids)
+            })
+
+            history = history[:10]
+            
+        with open(changelog_file, 'w') as f:
+            json.dump(history, f)
+
+
+    new_df.to_json('events.json', orient='records')
 
 if __name__ == "__main__":
     fetch_and_clean()
